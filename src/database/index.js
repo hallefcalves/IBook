598ea7a08bcf9a95033a0ec96af396a00b857e3f
hallefcalves <hallef28@gmail.com>
2021-10-01 09:34:37 -0300

    db

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 //Cria conexão com o banco de dados MongoDB
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/ibook-db');
+mongoose.connect('mongodb+srv://ibook:qRc9RGtcWEuuwkgA@ibook-cluster.ywy6w.mongodb.net/ibook?retryWrites=true&w=majority');
 mongoose.Promise = global.Promise;
 
 //Prepara o module.exports para envio dos dados ao MongoDB
