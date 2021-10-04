2cbc6fbf16e9822ff6b2a55dcd74e65af0b1078c
hallefcalves <hallef28@gmail.com>
2021-10-04 20:52:46 -0300

    secrets

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,9 @@
+const dotenv = require('dotenv')
 //Cria conexão com o banco de dados MongoDB
 const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/ibook-db');
+import Path from 'path';
+dotenv.config({path: Path.join(__dirname, '/config/.env')})
+mongoose.connect(process.env.CONNECTIONSTRING||"");
 mongoose.Promise = global.Promise;
 
 //Prepara o module.exports para envio dos dados ao MongoDB
