3b4cbaa991aed44eb1f6da97349caa454a188b1a
hallefcalves <48840344+hallefcalves@users.noreply.github.com>
2021-11-17 19:37:53 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -26,6 +26,12 @@ app.use(express.urlencoded({ extended: false }));
 
 app.options('*', cors());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+
 require('./controllers/usuariosController')(app);
 require('./controllers/bibliotecaController')(app);
 require('./controllers/entregadorController')(app);
