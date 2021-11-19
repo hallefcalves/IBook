9fa488b0aae2b3c9d1dac4b84e2de4936cce6d19
hallefcalves <48840344+hallefcalves@users.noreply.github.com>
2021-11-19 16:26:23 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,11 +20,12 @@ dotenv.config({path: Path.join(__dirname, '../config/.env')})
 const app = express();
 
 app.use(cors());
+
+app.options('*', cors());
+
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
-
-app.options('*', cors());
 
 require('./controllers/usuariosController')(app);
 require('./controllers/bibliotecaController')(app);
