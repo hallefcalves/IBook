aaa6372e672391f182a0922c22d20af4534785ba
hallefcalves <hallef28@gmail.com>
2021-11-19 19:54:30 -0300

    Acerto CORS

--- src/index.js
+++ src/index.js
@@ -16,11 +16,14 @@ dotenv.config({path: Path.join(__dirname, '../config/.env')})
 
 const app = express();
 
-app.use(cors({
-    origin: 'https://ibook-front.herokuapp.com'
-}));
-
-app.options('*', cors());
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
 
 app.use(express.json());
 
