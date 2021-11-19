c21412e5731bc13dce7d98b2a3d10d78e6965f8c
hallefcalves <hallef28@gmail.com>
2021-11-19 19:43:54 -0300

    Acerto CORS

--- src/index.js
+++ src/index.js
@@ -16,13 +16,9 @@ dotenv.config({path: Path.join(__dirname, '../config/.env')})
 
 const app = express();
 
-app.all('*', function(req, res, next) {
-    var origin = req.get('origin'); 
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
+app.use(cors());
+
+app.options('*', cors());
 
 app.use(express.json());
 
